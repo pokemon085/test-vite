41618cc7c0fb438cbf44df154947e492e5562470
pokemon085 <pokemon0850110@gmail.com>
2026-04-19 13:26:16 +0800

    fix header menu

--- src/components/pageHeader/index.vue
+++ src/components/pageHeader/index.vue
@@ -26,7 +26,7 @@
             <div class="search" @click="openSearch"></div>
             <div class="cart" @click="$router.push('/cart')">
                 <div v-if="getCartStore.cartTotal > 0" class="red-pot">
-                    {{ getCartStore.cartTotal }}
+                    {{ cartCount }}
                 </div>
             </div>
             <div class="login-wrap">
@@ -56,6 +56,70 @@
                 </div>
             </div>
         </div>
+
+        <div class="menu-wrap" @click="showDropDown = !showDropDown">
+            <i class="mdi mdi-menu"></i>
+        </div>
+    </div>
+
+    <div v-if="showDropDown" class="drop-down-wrap">
+        <div class="drop-down">
+            <div class="main-function">主功能</div>
+            <div class="main-function-wrap" @click="showDropDown = false">
+                <router-link class="path" to="/"
+                    ><i class="mdi mdi-home"></i>
+                    <div class="path-name">首頁</div>
+                </router-link>
+                <router-link class="path" to="/catalog"
+                    ><i class="mdi mdi-list-box-outline"></i>
+                    <div class="path-name">商品</div>
+                </router-link>
+                <router-link class="path" to="/faqs"
+                    ><i class="mdi mdi-comment-question"></i>
+                    <div class="path-name">常見問題</div>
+                </router-link>
+                <router-link class="path" to="/contact"
+                    ><i class="mdi mdi-information-slab-box"></i>
+                    <div class="path-name">聯繫我們</div>
+                </router-link>
+            </div>
+
+            <div class="main-function">其他</div>
+            <div class="main-function-wrap" @click="showDropDown = false">
+                <div class="search" @click="openSearch">
+                    <i class="mdi mdi-magnify"></i>
+                    搜尋
+                </div>
+                <div class="cart" @click="$router.push('/cart')">
+                    <i class="mdi mdi-cart-outline"></i>
+                    購物車
+                    <div v-if="getCartStore.cartTotal > 0" class="red-pot">
+                        {{ cartCount }}
+                    </div>
+                </div>
+            </div>
+
+            <div class="main-function">會員</div>
+            <div class="main-function-wrap" @click="showDropDown = false">
+                <div
+                    v-if="getUserStore.isLoginUser() === false"
+                    @click="$router.push('/login')"
+                    class="login"
+                >
+                    <i class="mdi mdi-login"></i>
+                    登入
+                </div>
+                <div v-else>
+                    <div class="email">
+                        信箱: {{ getUserStore.isLogin.email }}
+                    </div>
+                    <div class="history" @click="$router.push('/buyHistory')">
+                        購買紀錄
+                    </div>
+                    <div class="logout" @click="signOutHandler">登出</div>
+                </div>
+            </div>
+        </div>
     </div>
     <teleport to="body">
         <search v-if="showSearch" @close="closeHandler" />
@@ -64,7 +128,7 @@
 <script lang="ts" setup>
 import { userStore } from '@/store/user';
 import { useRouter } from 'vue-router';
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import toolTip from '@/components/toolTip/index.vue';
 import search from '@/components/search/index.vue';
 import { cartStore } from '@/store/cart';
@@ -75,6 +139,7 @@ const getUserStore = userStore();
 const getCartStore = cartStore();
 const showAccountDetail = ref(false);
 const showSearch = ref(false);
+const showDropDown = ref(false);
 const loginHandler = () => {
     showAccountDetail.value = !showAccountDetail.value;
 };
@@ -94,6 +159,10 @@ const closeHandler = () => {
 const openSearch = () => {
     showSearch.value = true;
 };
+
+const cartCount = computed(() => {
+    return getCartStore.cartTotal > 99 ? '99+' : getCartStore.cartTotal;
+});
 </script>
 <style lang="scss" scoped>
 .header {
@@ -102,6 +171,7 @@ const openSearch = () => {
     background: rgb(214, 185, 185);
     display: flex;
     justify-content: space-between;
+    align-items: center;
     position: fixed;
     top: 0;
 
@@ -211,6 +281,83 @@ const openSearch = () => {
             }
         }
     }
+
+    .menu-wrap {
+        display: none;
+        font-size: 30px;
+        margin-right: 10px;
+    }
+
+    .drop-down-wrap {
+        position: fixed;
+        width: 100%;
+        top: 50px;
+        background-color: #fff;
+
+        .drop-down {
+            padding: 5px 0;
+        }
+
+        .main-function {
+            font-size: 18px;
+            padding: 5px;
+            background-color: bisque;
+        }
+
+        .path {
+            display: flex;
+            align-items: center;
+            text-decoration: none;
+            font-size: 12px;
+            min-width: 60px;
+            color: #333234;
+            font-size: 16px;
+            padding: 5px 10px;
+
+            > i {
+                color: #333234;
+                text-align: center;
+                font-size: 20px;
+                margin-right: 8px;
+            }
+        }
+
+        .main-function-wrap {
+            .search,
+            .cart,
+            .login,
+            .email,
+            .history,
+            .logout {
+                padding: 10px;
+            }
+
+            .cart {
+                display: flex;
+                align-items: center;
+
+                .red-pot {
+                    position: relative;
+                    margin-left: 10px;
+                    color: #fff;
+                    &::after {
+                        content: '';
+                        position: absolute;
+                        top: 50%;
+                        left: 50%;
+                        transform: translate(-50%, -50%);
+                        border-radius: 50%;
+                        background-color: #f00;
+                        min-width: 20px;
+                        width: auto;
+                        height: 20px;
+                        font-size: 12px;
+                        z-index: -1;
+                    }
+                }
+            }
+        }
+    }
 }
 
 .list-item {
@@ -219,9 +366,24 @@ const openSearch = () => {
 
 @media screen and (max-width: 767px) {
     /* 在螢幕寬度小於 767px 時使用以下 CSS 規則 */
-    .logo-wrap {
-        .logo-name {
+
+    .header {
+        .logo-wrap {
+            .logo-name {
+                display: none;
+            }
+        }
+
+        .list {
             display: none;
+        }
+
+        .function-icon {
+            display: none;
+        }
+
+        .menu-wrap {
+            display: block;
         }
     }
 }
